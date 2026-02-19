0b6c2d71eaf392b8c24bb8107ef424cb010d0476
Michał Jaśkiewicz <michaljaskiewicz.developer@protonmail.com>
2026-02-19 15:39:39 +0100

    wip

--- backend/src/Application/Common/BaseResponse.cs
+++ backend/src/Application/Common/BaseResponse.cs
@@ -1,7 +1,9 @@
-namespace Application.Common;
+using System.Text.Json.Serialization;
 
+namespace Application.Common;
 
 public abstract record BaseResponse
 {
+    [JsonPropertyOrder(int.MinValue)]
     public required ulong Id { get; init; }
 }
